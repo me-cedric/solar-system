e5b79b39ddcc60c979b5d4e986034922c6e73cc6
Cédric Meyer <mecedricod@gmail.com>
2025-02-19 06:32:01 +0100

    init stars and planets

---
+++ src/app/components/stars.tsx
@@ -0,0 +1,118 @@
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./stars.module.scss";
+
+interface IProps {
+  name: string;
+  [key: string]: React.CSSProperties | string;
+}
+
+const Keyframes = (props: IProps) => {
+  const toCss = (cssObject: React.CSSProperties | string) =>
+    typeof cssObject === "string"
+      ? cssObject
+      : Object.keys(cssObject).reduce((accumulator, key) => {
+          const cssKey = key.replace(/[A-Z]/g, (v) => `-${v.toLowerCase()}`);
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const cssValue = (cssObject as any)[key].toString().replace("'", "");
+          return `${accumulator}${cssKey}:${cssValue};`;
+        }, "");
+
+  return (
+    <style>
+      {`@keyframes ${props.name} {
+        ${Object.keys(props)
+          .map((key) => {
+            return ["from", "to"].includes(key)
+              ? `${key} { ${toCss(props[key])} }`
+              : /^_[0-9]+$/.test(key)
+              ? `${key.replace("_", "")}% { ${toCss(props[key])} }`
+              : "";
+          })
+          .join(" ")}
+      }`}
+    </style>
+  );
+};
+
+function getWindowDimensions() {
+  if (typeof window !== "undefined") {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
+  return {
+    width: 0,
+    height: 0,
+  };
+}
+
+export function useStars() {
+  const getStyles = () => {
+    const { height } = getWindowDimensions();
+    const starStyle: React.CSSProperties = {
+      boxShadow:
+        `${random(height * 2)}px ${random(height * 2)}px #FFF` +
+        [...Array(700)]
+          .map(() => ` , ${random(height * 2)}px ${random(height * 2)}px #FFF`)
+          .join(""),
+      animation: "animStar 50s linear infinite",
+    };
+    const starStyle2: React.CSSProperties = {
+      boxShadow:
+        `${random(height * 2)}px ${random(height * 2)}px #FFF` +
+        [...Array(200)]
+          .map(() => ` , ${random(height * 2)}px ${random(height * 2)}px #FFF`)
+          .join(""),
+      animation: "animStar 100s linear infinite",
+    };
+    const starStyle3: React.CSSProperties = {
+      boxShadow:
+        `${random(height * 2)}px ${random(height * 2)}px #FFF` +
+        [...Array(100)]
+          .map(() => ` , ${random(height * 2)}px ${random(height * 2)}px #FFF`)
+          .join(""),
+      animation: "animStar 150s linear infinite",
+    };
+    return {
+      starStyle,
+      starStyle2,
+      starStyle3,
+      transform: `translateY(-${height * 2}px)`,
+    };
+  };
+  const [stars, setStars] = useState(getStyles());
+
+  useEffect(() => {
+    function handleResize() {
+      setStars(getStyles());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return stars;
+}
+
+function random(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+export default function Stars() {
+  const { starStyle, starStyle2, starStyle3, transform } = useStars();
+  return (
+    <>
+      <Keyframes
+        name="animStar"
+        from={{ transform: "translateY(0px)" }}
+        to={{ transform }}
+      />
+      <div className={styles.stars} style={starStyle}></div>
+      <div className={styles.stars2} style={starStyle2}></div>
+      <div className={styles.stars3} style={starStyle3}></div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,183 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import dynamic from "next/dynamic";
+import React from "react";
+import styles from "./page.module.scss";
+
+const sunSize = 2.5; // rem
+const sunActualSizeKm = 1400000; // 1.4M km
+const earthRotationLength = 4.1; // 4.1s for 365d TODO editable later
+const earthRotationDays = 365;
+const earthRotationDuration = 1; // days
+const scale = 40;
+const durationOfDay = 1; // seconds
+const rotationOn = false; // seconds
+
+const defaultSolarSystem: PlanetProps[] = [
+  {
+    name: "Mercure",
+    size: 4880,
+    daysToOrbit: 87.97,
+    daysToFullRotation: 59,
+    distanceToCenter: 58000000,
+    reverseRotationDirection: false,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/220px-Mercury_in_color_-_Prockter07-edit1.jpg",
+  },
+  {
+    name: "Venus",
+    size: 12100,
+    daysToOrbit: 224.7,
+    daysToFullRotation: 243,
+    distanceToCenter: 108000000,
+    reverseRotationDirection: false,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Venus-real_color.jpg/220px-Venus-real_color.jpg",
+  },
+  {
+    name: "Earth",
+    size: 12800,
+    daysToOrbit: 365.26,
+    daysToFullRotation: 1,
+    distanceToCenter: 150000000,
+    reverseRotationDirection: false,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/The_Earth_seen_from_Apollo_17.jpg/220px-The_Earth_seen_from_Apollo_17.jpg",
+    moons: [
+      {
+        name: "Lune",
+        size: 12742,
+        daysToOrbit: 27.3,
+        daysToFullRotation: 27.3,
+        distanceToCenter: 150000000,
+        reverseRotationDirection: false,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/220px-FullMoon2010.jpg",
+      },
+    ],
+  },
+  {
+    name: "Mars",
+    size: 6800,
+    daysToOrbit: 686.98,
+    daysToFullRotation: 1.02,
+    distanceToCenter: 228000000,
+    reverseRotationDirection: false,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/220px-OSIRIS_Mars_true_color.jpg",
+  },
+  {
+    name: "Jupiter",
+    size: 142000,
+    daysToOrbit: 4332,
+    daysToFullRotation: 0.42,
+    distanceToCenter: 778000000,
+    reverseRotationDirection: false,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/8/80/Jupiter_detail_voyager2.jpg",
+  },
+  {
+    name: "Saturne",
+    size: 120000,
+    daysToOrbit: 10761,
+    daysToFullRotation: 0.44,
+    distanceToCenter: 1430000000,
+    reverseRotationDirection: false,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Saturn_-_HST_2019-06-20_full_size.jpg/220px-Saturn_-_HST_2019-06-20_full_size.jpg",
+  },
+  {
+    name: "Uranus",
+    size: 51800,
+    daysToOrbit: 30685,
+    daysToFullRotation: 0.72,
+    distanceToCenter: 2870000000,
+    reverseRotationDirection: false,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Uranus2.jpg/220px-Uranus2.jpg",
+  },
+  {
+    name: "Neptune",
+    size: 49500,
+    daysToOrbit: 60191,
+    daysToFullRotation: 0.67,
+    distanceToCenter: 4500000000,
+    reverseRotationDirection: false,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Neptune_-_Voyager_2_%2829347980845%29_flatten_crop.jpg/220px-Neptune_-_Voyager_2_%2829347980845%29_flatten_crop.jpg",
+  },
+];
+class PlanetProps {
+  name!: string;
+  size!: number;
+  daysToOrbit!: number;
+  daysToFullRotation!: number;
+  distanceToCenter!: number;
+  reverseRotationDirection!: boolean;
+  image!: string;
+  moons?: PlanetProps[];
+}
+function Planet(props: Readonly<PlanetProps>) {
+  const planetSize = (sunSize / sunActualSizeKm) * props.size * scale;
+  const planetRotationLength =
+    (earthRotationLength / earthRotationDays) * props.daysToOrbit;
+  const planetRotationDuration =
+    (earthRotationDuration / durationOfDay) * props.daysToFullRotation;
+  const planetDistanceToCenter =
+    ((sunSize / sunActualSizeKm) * props.distanceToCenter) / scale;
+
+  const planetOrbitStyle: React.CSSProperties = {
+    width: `calc(${planetDistanceToCenter * 2}rem + 4px)`,
+    height: `calc(${planetDistanceToCenter * 2}rem + 4px)`,
+    animation: `movement ${planetRotationLength}s linear infinite`,
+    left: `calc(var(--sun-left) - ${planetDistanceToCenter}rem + var(--sun-radius))`,
+    top: `calc(var(--sun-top) - ${planetDistanceToCenter}rem + var(--sun-radius))`,
+  };
+  const planetContentStyle: React.CSSProperties = {
+    position: "absolute",
+    width: `${planetSize}rem`,
+    height: `${planetSize}rem`,
+    backgroundImage: `url(${props.image})`,
+    backgroundSize: `100%`,
+    top: `calc(50% - ${planetSize / 2}rem)`,
+    left: `-${planetSize / 2}rem`,
+    animation: rotationOn
+      ? `movement ${planetRotationDuration}s linear infinite`
+      : undefined,
+    animationDirection: rotationOn
+      ? props.reverseRotationDirection
+        ? "reverse"
+        : "normal"
+      : undefined,
+  };
+  return (
+    <div className={styles.planet} style={planetOrbitStyle} title={props.name}>
+      <div className={styles["planet-image"]} style={planetContentStyle} />
+    </div>
+  );
+}
+
+const Stars = dynamic(() => import("./components/stars"), { ssr: false });
 
 export default function Home() {
+  const planets = defaultSolarSystem.map((planet) => (
+    <Planet
+      key={planet.name}
+      name={planet.name}
+      size={planet.size}
+      daysToOrbit={planet.daysToOrbit}
+      daysToFullRotation={planet.daysToFullRotation}
+      reverseRotationDirection={planet.reverseRotationDirection}
+      distanceToCenter={planet.distanceToCenter}
+      image={planet.image}
+    />
+  ));
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <Stars></Stars>
+      <div className={styles.system}>
+        <div className={styles.sun}></div>
+        {planets}
+      </div>
+    </>
   );
 }
