cfd448ecfdf57cbb7be21687e8ee37c448554ca2
Cédric Meyer <mecedricod@gmail.com>
2025-02-20 02:05:41 +0100

    chore: cleanup and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Solar",
   description: "A me-cedric project",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
